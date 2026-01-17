488fa611a171987f2d3a4f5c03a23e413f18fa8a
Hossein <hosseinheydari992020@gmail.com>
2026-01-17 09:34:21 +0330

    update project

---
+++ main/main.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+class LinearRegression_scratch:
+    '''
+    '''
+    def fit(self, x_train, y_train):
+        def Gradian_desent():
+            pass
+        pass
+    
+    def predict(self, x_test):
+        pass
+    
