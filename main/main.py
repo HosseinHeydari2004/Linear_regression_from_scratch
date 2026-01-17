84cc725cc0c0903f14bcef4c3f3fe1aa332f1eca
Hossein <hosseinheydari992020@gmail.com>
2026-01-17 23:29:14 +0330

    update project

--- main/main.py
+++ main/main.py
@@ -1,13 +1,19 @@
-import numpy as np 
+import numpy as np
+import pandas as pd
+
 
 class LinearRegression_scratch:
-    '''
-    '''
-    def fit(self, x_train, y_train):
+    """ """
+
+    def fit(self, x_train: np.array | pd.DataFrame, y_train: np.array | pd.DataFrame):
+        total_feature = x_train.shape[1]
+        y = x_train
+        y_real = None
+
         def Gradian_desent():
             pass
+
         pass
-    
+
     def predict(self, x_test):
         pass
-    
